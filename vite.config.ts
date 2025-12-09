3bc0f4b3e60d52fc39bff1cfca6b94834a4b6d18
we-expand <cleber.couto@we-expand.com>
2025-12-09 10:28:11 +0000

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+typescript
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  }
+})
