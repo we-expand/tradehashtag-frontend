17d26341c6faaa7f09fa29aa0db35c666fdea388
we-expand <cleber.couto@we-expand.com>
2025-12-09 10:36:25 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 typescript
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json']
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   build: {
     outDir: 'dist',
